1baaad88d9c669ebaf66eabbfacb708c0722e6ea
雷薇 <leiwei183@163.com>
2017-03-22 16:31:28 +0800

    SimpleFormula

---
+++ Input Dialog/Input Dialog.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Activities;
+using System.ComponentModel;
+
+namespace Vivi.Package.Test
+{
+    public class InputDialog : CodeActivity
+    {
+        [Category("Input")]
+        [RequiredArgument]
+        public InArgument<double> FirstNumber { get; set; }
+
+        [Category("Input")]
+        public InArgument<double> SecondNumber { get; set; }
+
+        [Category("Output")]
+        public OutArgument<double> ResultNumber { get; set; }
+
+        protected override void Execute(CodeActivityContext context)
+        {
+            var firstNumber = FirstNumber.Get(context);
+            var secondNumber = SecondNumber.Get(context);
+            var result = Math.Pow(firstNumber + secondNumber, 2);
+            ResultNumber.Set(context, result);
+            //throw new NotImplementedException();
+        }
+    }
+}
